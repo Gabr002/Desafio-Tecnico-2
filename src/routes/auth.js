e38a85282bf98ce38f1f986d135514a9357a40b7
Gabriel Dantas <gdantaslopes62@gmail.com>
2023-11-24 15:22:26 -0300

    Para a criação de novos usuarios

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,3 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { signUp, signIn, getUser } = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
+
+
+router.post('/signup', signUp);
