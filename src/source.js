cdb1cebb1086e90d14427168af2be3f95091ca57
Gabriel Dantas <gdantaslopes62@gmail.com>
2023-11-24 11:43:54 -0300

    conexão com o banco

--- src/source.js
+++ src/source.js
@@ -2,3 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
+
+require('dotenv').config();
+
+// Conexão com o banco de dados usando o mongoose
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on('error', console.error.bind(console, 'Erro de conexão ao MongoDB:'));
+mongoose.connection.once('open', () => {
+  console.log('Conectado ao MongoDB!');
+});
