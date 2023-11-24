0e06c40c21e828e1f2c6e7674ce7d976434f39ce
Gabriel Dantas <gdantaslopes62@gmail.com>
2023-11-24 15:31:40 -0300

    Criptografar senha

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,8 +6,10 @@ const signUp = async (req, res) => {
     try {
       const { nome, email, senha, telefones } = req.body;
   
-      // Verificar se o e-mail já está cadastrado
       const existingUser = await User.findOne({ email });
       if (existingUser) {
         return res.status(400).json({ mensagem: 'E-mail já existente' });
       }
+
+       // Criptografar senha
+    const hashedPassword = await bcrypt.hash(senha, 10);
