5c757a0c06c0da291109ae0f78f82d39dce4b5f0
Gabriel Dantas <gdantaslopes62@gmail.com>
2023-11-24 15:55:37 -0300

    criação da folder authMiddleware

---
+++ src/midldlewares/authMiddleware.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const authMiddleware = async (req, res, next) => {
+  const token = req.header('Authorization') && req.header('Authorization').replace('Bearer ', '');
+
+  if (!token) {
+    return res.status(401).json({ mensagem: 'Não autorizado' });
+  }
+
+  try {
