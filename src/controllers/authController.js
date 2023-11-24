912252a25a4371a564c89685bf26c134fc302d07
Gabriel Dantas <gdantaslopes62@gmail.com>
2023-11-24 15:43:00 -0300

    Encontrar usuário pelo e-mail

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,38 +3,49 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const signUp = async (req, res) => {
-    try {
-      const { nome, email, senha, telefones } = req.body;
-  
-      const existingUser = await User.findOne({ email });
-      if (existingUser) {
-        return res.status(400).json({ mensagem: 'E-mail já existente' });
-      }
+    try{
+        const { nome, email, senha, telefones } = req.body;
+        
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+          return res.status(400).json({ mensagem: 'E-mail já existente' });
+        }
 
     
-    const hashedPassword = await bcrypt.hash(senha, 10);
+        const hashedPassword = await bcrypt.hash(senha, 10);
 
-    const newUser = new User({
-        nome,
-        email,
-        senha: hashedPassword,
-        telefones,
-      });
+        const newUser = new User({
+            nome,
+            email,
+            senha: hashedPassword,
+            telefones,
+          });
 
-    await newUser.save();
+        await newUser.save();
 
-    const token = generateToken(newUser);
-    
-    // Responder com sucesso
-    res.status(201).json({
-        id: newUser._id,
-        dataCriacao: newUser.dataCriacao,
-        dataAtualizacao: newUser.dataAtualizacao,
-        ultimoLogin: newUser.ultimoLogin,
-        token,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ mensagem: 'Erro interno do servidor' });
+        const token = generateToken(newUser);
+      
+        // Responder com sucesso
+        res.status(201).json({
+            id: newUser._id,
+            dataCriacao: newUser.dataCriacao,
+            dataAtualizacao: newUser.dataAtualizacao,
+            ultimoLogin: newUser.ultimoLogin,
+            token,
+          });
+    }catch (error) {
+        console.error(error);
+        res.status(500).json({ mensagem: 'Erro interno do servidor' });
     }
 };
+
+const signIn = async (req, res) => {
+    try {
+      const { email, senha } = req.body;
+
+      // Encontrar usuário pelo e-mail
+    const user = await User.findOne({ email });
+
+    if (!user || !(await bcrypt.compare(senha, user.senha))) {
+      return res.status(401).json({ mensagem: 'Usuário e/ou senha inválidos' });
+    }
