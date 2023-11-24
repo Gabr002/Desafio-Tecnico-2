171c555114f472d9efe2f2bcb6ea0b41dc248ad9
Gabriel Dantas <gdantaslopes62@gmail.com>
2023-11-24 15:30:19 -0300

    Verificando se o e-mail já está cadastrado

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,13 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+
+const signUp = async (req, res) => {
+    try {
+      const { nome, email, senha, telefones } = req.body;
+  
+      // Verificar se o e-mail já está cadastrado
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ mensagem: 'E-mail já existente' });
+      }
