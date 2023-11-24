046a0443dd1d404614c18fd60ce78fad5f449cf2
Gabriel Dantas <gdantaslopes62@gmail.com>
2023-11-24 15:56:37 -0300

    Encontrar usuário pelo ID do token

--- src/midldlewares/authMiddleware.js
+++ src/midldlewares/authMiddleware.js
@@ -10,3 +10,5 @@ const authMiddleware = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    
+    const user = await User.findById(decoded.id);
